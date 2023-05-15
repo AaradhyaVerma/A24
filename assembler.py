b52982786c324d117fae42da78962a1f93ee9eef
adimoza2211 <adimoza2211@gmail.com>
2023-05-15 21:56:49 +0530

    fixed flags

--- assembler.py
+++ assembler.py
@@ -1,4 +1,4 @@
-reg_bin = {"R0": 0, "R1": 1, "R2": 2, "R3": 3, "R4": 4, "R5": 5, "R6": 6}
+reg_bin = {"R0": 0, "R1": 1, "R2": 2, "R3": 3, "R4": 4, "R5": 5, "R6": 6, "FLAGS":7}
 mem_bin = {}
 
 #creating a decimal to binary converter which takes the decimal and the number of bits as input
