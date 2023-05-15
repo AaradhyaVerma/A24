a96b3c29e718ae82387d76b55929c05506bfe921
adimoza2211 <adimoza2211@gmail.com>
2023-05-15 10:17:18 +0530

    binfunctionworks

--- assembler.py
+++ assembler.py
@@ -1,1 +1,14 @@
 
+
+#creating a decimal to binary converter which takes the decimal and the number of bits as input
+def returnbin(num,bits):
+    s = bin(num)
+    s = s[2:]
+    if(len(s) > bits):
+        print("number cannot be represented in given number of bits")
+        return
+    for i in range(bits - len(s)):
+        s = '0' + s
+    return s
+
+
